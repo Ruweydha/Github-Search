7783adb2da6ee9b98e1f2c4ac165bfbd8d8a8cca
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-15 14:16:50 +0300

    Created a component to display repo details

---
+++ src/app/diplay-repos/diplay-repos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diplay-repos',
+  templateUrl: './diplay-repos.component.html',
+  styleUrls: ['./diplay-repos.component.css']
+})
+export class DiplayReposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
