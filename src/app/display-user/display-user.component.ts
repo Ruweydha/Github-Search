ff77b2351fc0d91650cf9fbac0e18e14dd8daa9e
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-15 14:07:33 +0300

    Added code for requesting user details from the api

--- src/app/display-user/display-user.component.ts
+++ src/app/display-user/display-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { HttpClient } from '@angular/common/http';
+import { UsersHttpRequestService } from '../users-http-request.service';
 
 @Component({
   selector: 'app-display-user',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayUserComponent implements OnInit {
 
-  constructor() { }
+  user!:User
 
-  ngOnInit(): void {
+  searchUserName(username:string){
+    this._usersRequest.userRequest(username);
+    this.user = this._usersRequest.user
+    
+  }
+
+  constructor(private http:HttpClient, private _usersRequest:UsersHttpRequestService) { }
+
+  ngOnInit(){
+    interface ApiResponse{
+      login:string; 
+      followers:number;
+      following:number; 
+      avatar_url:string;
+      created_at:Date;
+    }
+    this.http.get<ApiResponse>("https://api.github.com/users").subscribe(data=>{
+      this.user = new User(data.login, data.followers, data.following, data.avatar_url, data.created_at)
+    }, err=>{
+      console.log("An error has occured")
+    })
   }
 
 }
