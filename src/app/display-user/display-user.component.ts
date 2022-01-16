9782849629977895046d29c6ecbb81ca24977d90
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-16 15:55:22 +0300

    Removed unnecessary code

--- src/app/display-user/display-user.component.ts
+++ src/app/display-user/display-user.component.ts
@@ -20,19 +20,7 @@ export class DisplayUserComponent implements OnInit {
 
   constructor(private http:HttpClient, private _usersRequest:UsersHttpRequestService) { }
 
-  ngOnInit(){
-    interface ApiResponse{
-      login:string; 
-      followers:number;
-      following:number; 
-      avatar_url:string;
-      created_at:Date;
-    }
-    this.http.get<ApiResponse>("https://api.github.com/users").subscribe(data=>{
-      this.user = new User(data.login, data.followers, data.following, data.avatar_url, data.created_at)
-    }, err=>{
-      console.log("An error has occured")
-    })
-  }
+
+  ngOnInit(): void {}
 
 }
