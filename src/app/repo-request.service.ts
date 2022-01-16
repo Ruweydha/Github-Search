f9fbeed21a1342ed3306e77366f1bf29388534c9
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-16 21:50:16 +0300

    Was able to use the environment file for the link

--- src/app/repo-request.service.ts
+++ src/app/repo-request.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Repository } from './repository';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class RepoRequestService {
 
 
   repoRequest(username:string):Observable<Repository[]>{
-    return this.http.get<Repository[]>("https://api.github.com/users/" + username + "/repos")
+    return this.http.get<Repository[]>(environment.apiUrl +/users/ + username + "/repos")
    }
 
   constructor(private http:HttpClient) { }
