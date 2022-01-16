f4611a1e6e1633e1864aa20f7993f1d08feede2b
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-17 01:27:52 +0300

    Wrote code to show how long ago a user created an account

--- src/app/duration.pipe.ts
+++ src/app/duration.pipe.ts
@@ -1,42 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment'
 
 @Pipe({
   name: 'duration'
 })
 export class DurationPipe implements PipeTransform {
 
-  transform(value: any): number {
-    let time:any = "";
-    let today:any = new Date();
-    const secondsInDay = 86400;
-    const secondsInHour = 3600;
-    var timeDifference = Math.abs(value - today)/1000;
-    var numberOfDays = Math.floor(timeDifference/secondsInDay);
-
-    if(numberOfDays >0){
-      time += `${Math.floor(numberOfDays)}Days `;
-
-    }
-    timeDifference -= numberOfDays * secondsInDay;
-    var numberOfHours = Math.floor(timeDifference/secondsInHour);
-
-   if(numberOfHours > 0){
-      time += `${Math.floor(numberOfHours)}Hours `
-    }
-    timeDifference -= numberOfHours * secondsInHour;
-    var minutes = Math.floor(timeDifference/60);
-
-    if(minutes > 0){
-      time += `${Math.floor(minutes)}Minutes `
-    }
-    timeDifference -= minutes *60;
-    var seconds = timeDifference %60;
-
-   if(seconds > 0){
-      time += `${Math.floor(seconds)}Seconds `
-    }
-
-      return time;
+  transform(value: any): any {
+    var timeElapsed = moment(value).fromNow()
+    return timeElapsed
   }
-
+   
 }
