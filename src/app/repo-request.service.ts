2e3174f7e0035a2e48a8f3830f184bce0e7feeab
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-16 18:04:59 +0300

    Wrote code for requesting repo details

--- src/app/repo-request.service.ts
+++ src/app/repo-request.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Repository } from './repository';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoRequestService {
 
-  constructor() { }
+
+  repoRequest(username:string):Observable<Repository[]>{
+    return this.http.get<Repository[]>("https://api.github.com/users/" + username + "/repos")
+   }
+
+  constructor(private http:HttpClient) { }
 }
