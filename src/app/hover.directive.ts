e2c849923d54ce70f1cb355061933b9090bb62b6
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-16 23:11:00 +0300

    Changed the background color on hover

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { getCurrencySymbol } from '@angular/common';
+import { Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+
+  @HostListener("mouseover") onMouseOver(){
+    this.backgroundColor("rgb(29, 61, 98)")
+    this.elem.nativeElement.style.color ="white"
+  }
+  @HostListener("mouseleave") onMouseLeave(){
+    this.backgroundColor("white")
+    this.elem.nativeElement.style.color = "black"
+   
+  }
+
+  private backgroundColor(action:string){
+    this.elem.nativeElement.style.backgroundColor = action;
+  }
 
 }
