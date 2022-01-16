1e4f2904f8097523989616dd88aafd5beba34534
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-16 21:48:55 +0300

    Created routing for the two pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DisplayUserComponent } from './display-user/display-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomePageComponent},
+  {path:'users',component:DisplayUserComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
