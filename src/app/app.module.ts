fa8e1f7bce489b5ad35aaba2246189a8b636ea39
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-18 13:32:16 +0300

    Updated itself after generating the components and classes, imported formsmodule and httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
+import { DisplayUserComponent } from './display-user/display-user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DurationPipe } from './duration.pipe';
+import { HoverDirective } from './hover.directive';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SearchComponent
+    SearchComponent,
+    DisplayUserComponent,
+    DurationPipe,
+    HoverDirective,
+    HomePageComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
